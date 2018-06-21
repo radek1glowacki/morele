c20d3471a5fd07c17a6cc6e5a22e444deaf7b59a
Radosław <radek1glowacki@gmail.com>
2018-06-21 19:52:00 +0200

    Zmiana w moreli2

--- pesel.py
+++ pesel.py
@@ -1,5 +1,5 @@
 PESEL=input()
-suma = 9*int(PESEL[0]) + 7*int(PESEL[1]) + 3*int(PESEL[2]) + 1*int(PESEL[3]) + 9*int(PESEL[4]) + 7*int(PESEL[5]) + 3*int(PESEL[6]) + 1*int(PESEL[7]) + 9*int(PESEL[8]) + 7*int(PESEL[9]);
+suma = 9*(PESEL[0]) + 7*int(PESEL[1]) + 3*int(PESEL[2]) + 1*int(PESEL[3]) + 9*int(PESEL[4]) + 7*int(PESEL[5]) + 3*int(PESEL[6]) + 1*int(PESEL[7]) + 9*int(PESEL[8]) + 7*int(PESEL[9]);
 if suma%10 == int(PESEL[len(PESEL)-1]):
 else:
-  print("8")
+  print("15")
